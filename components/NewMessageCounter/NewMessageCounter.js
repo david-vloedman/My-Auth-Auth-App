1c61a44fa928d92aa24c74e4668dcb6b47cea2fd
David <dvloedman@protonmail.com>
2021-04-06 17:28:34 -0700

    Add onClick function to mail indicator that directs to /friends for now

--- components/NewMessageCounter/NewMessageCounter.js
+++ components/NewMessageCounter/NewMessageCounter.js
@@ -2,12 +2,23 @@ import { connect } from 'react-redux'
 import MailIcon from '@material-ui/icons/Mail'
 import Box from '@material-ui/core/Box'
 import Badge from '@material-ui/core/Badge'
+import IconButton from '@material-ui/core/IconButton'
+import { useRouter } from 'next/router'
 
 const NewMessageCounter = ({ count }) => {
+
+	const router = useRouter()
+
+	const onClick = () => {
+		router.push('/friends')
+	}
+
 	return (
+		<IconButton onClick={onClick}>
 		<Badge badgeContent={count}>
 			<MailIcon />
 		</Badge>
+		</IconButton>
 	)
 }
 
