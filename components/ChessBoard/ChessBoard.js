be5bb0fbbc4ac7a696d695b775d0ed6aaa949c15
David <dvloedman@protonmail.com>
2021-06-23 10:03:52 -0700

    Add chessboard orientation flipping based on the users color

--- components/ChessBoard/ChessBoard.js
+++ components/ChessBoard/ChessBoard.js
@@ -2,16 +2,17 @@ import { Box } from '@material-ui/core'
 import dynamic from 'next/dynamic'
 import { connect } from 'react-redux'
 
-export function ChessBoard({ fenString }) {
+export function ChessBoard({ fenString, orientation }) {
 	const Chessboard = dynamic(() => import('chessboardjsx'), { ssr: false })
 
 	return (
 		<Box id='chessBoard' ml={'auto'} mr={'auto'}>
-			<Chessboard width={340} position={fenString} />
+			<Chessboard width={340} position={fenString} orientation={orientation} />
 		</Box>
 	)
 }
 
-export default connect((state) => ({ fenString: state.chess.game.fenString }))(
-	ChessBoard
-)
+export default connect((state) => ({
+	fenString: state.chess.game.fenString,
+	orientation: state.chess.game.players.white === state.user.id ? 'white' : 'black',
+}))(ChessBoard)
